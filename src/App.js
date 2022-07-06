a5bb338e5a5a3249c40a5c6f9f6cc356e2e761ca
Piotr Matlak <piotr.matlak.contact@gmail.com>
2022-07-06 10:40:53 +0200

    add styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import "./App.css";
+import "./sass/App.scss";
 
 function App() {
   const addNote = () => {};
   return (
-    <div className="App">
+    < >
+      <div className="background">
+        <div className="to-do">
+          <h2>To do</h2>
+        </div>
+        <div className="in-progress">
+          <h2>In progress</h2>
+        </div>
+        <div className="done">
+          <h2>Done</h2>
+        </div>
+      </div>
+      <div className="App">
       <h1>Sticky notes</h1>
-      <form className="note-form" onSubmit={addNote}></form>
-    </div>
+      <form className="note-form" onSubmit={addNote}>
+        <textarea placeholder="Create a new note..."></textarea>
+        <button>Add</button>
+      </form>
+      </div>
+    </>
   );
 }
 
